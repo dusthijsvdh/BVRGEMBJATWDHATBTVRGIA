a622465a88cc8532c01e70653a6cf363752961e2
Thijs van der Heijden <thijsvdh12@gmail.com>
2021-06-27 11:37:56 +0200

    Sounds and model

--- src/sound.js
+++ src/sound.js
@@ -5,11 +5,6 @@ AFRAME.registerComponent('soundable', {
 
         this.el.addEventListener("mouseenter", function (event) {
             cursor.setAttribute("color", "blue");
-            console.log(elementodes.components.sound)
-            if (elementodes.components.sound.firstTime || elementodes.components.sound.firstTime == undefined) {
-                elementodes.components.sound.stopSound();
-                elementodes.components.sound.firstTime = false;
-            }
         });
 
         this.el.addEventListener("mouseleave", function (event) {
@@ -17,10 +12,7 @@ AFRAME.registerComponent('soundable', {
         });
 
         this.el.addEventListener("click", function (event) {
-            if (!elementodes.components.sound.isPlaying) {
-                console.log("aaaaaaaaaaaaaaaaaa")
-                elementodes.components.sound.playSound();
-            }
+            elementodes.components.sound.playSound();
         });
     },
 });
